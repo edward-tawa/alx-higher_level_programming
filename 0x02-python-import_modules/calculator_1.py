04ee6d1aecb756ba090250095f2afbc799959238
Edward <tagumatawa@gmail.com>
2023-08-12 05:58:16 -0700

    1-calculation.py

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 def add(a, b):
     """My addition function
@@ -37,4 +36,17 @@ def mul(a, b):
         The return value. a * b
     """
     return (a * b)
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
 
