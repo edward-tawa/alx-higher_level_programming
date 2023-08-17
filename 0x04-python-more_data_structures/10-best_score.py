56697c83fb1b183cb3209c496f0d3b52316f3c97
Edward <tagumatawa@gmail.com>
2023-08-17 10:28:16 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    """ returns a key with the biggest integer value"""
+    """ searches a key with the biggest integer value"""
     if not a_dictionary:
         return (None)
 
