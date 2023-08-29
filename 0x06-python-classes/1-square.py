8f40a274367e0182ccefbf11ce9ae41113bd9bf7
Edward <tagumatawa@gmail.com>
2023-08-29 03:48:51 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a square """
+
+
+class Square:
+    """represent a square"""
+
+    def __init__(self, size):
+        """ Instantiate a new square
+
+        Args:
+            size(int): the size of the square
+        """
+        self.__size = size
